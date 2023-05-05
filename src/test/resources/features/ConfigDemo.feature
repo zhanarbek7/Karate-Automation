5fde2053b7cbedae444b60df3720955a1c1753e8
zhanarbekabdurasulov <zhanarbek.a7@gmail.com>
2023-05-05 11:55:34 -0500

    1. Changed pom.xml 2. Create config and feature files for Karate features

---
+++ src/test/resources/features/ConfigDemo.feature
@@ -0,0 +1,4 @@
+Feature: Config Demo
+
+  Scenario: Config Demo 1
+    Given print name
---
+++ src/test/resources/karate-config.js
@@ -0,0 +1,6 @@
+function fn(){
+    var config = {
+        name : "Zhanarbek"
+    };
+    return config;
+}
