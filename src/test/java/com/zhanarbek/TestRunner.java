d445f163aa819d85ef2c304976b7e4ae40845a8d
zhanarbekabdurasulov <zhanarbek.a7@gmail.com>
2023-05-04 21:13:07 -0500

    Fixed features location issue

--- src/test/java/com/zhanarbek/TestRunner.java
+++ src/test/java/com/zhanarbek/TestRunner.java
@@ -1,10 +1,10 @@
 package com.zhanarbek;
 
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
-import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
-@CucumberOptions(features = "src/test/resources/features")
+@KarateOptions(features = "classpath:features")
 public class TestRunner {
 }
